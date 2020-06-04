5d93f0404f0bd7844173e8c85b1812ce72b68cb8
gdukes707 <Gdukes707@gmail.com>
2020-06-04 11:16:16 -0700

    server set up

---
+++ models/index.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const connectionString = "mongodb://localhost:27017/e-commerce";
+
+const configOptions = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+};
+
+mongoose
+  .connect(connectionString, configOptions)
+  .then(() => console.log("MongoDB connection successful"))
+  .catch((err) => console.log(`MongoDB connection error: ${err}`));
