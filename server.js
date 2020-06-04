0148dfb6ff53f1c65e434236599680fed436ff7f
gdukes707 <Gdukes707@gmail.com>
2020-06-04 11:25:46 -0700

    DB connected

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require("./models");
 
 const port = process.env.PORT || 4000;
 const app = express();
@@ -7,5 +8,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors());
+
+// app.use("/api/v1/users", routes.users);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
