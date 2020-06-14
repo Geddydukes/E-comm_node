3a8dd4be72cdde4da51fbc25f94f22ac714980d2
gdukes707 <Gdukes707@gmail.com>
2020-06-14 15:17:39 -0700

    ready for heroku deployment

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
-const connectionString = "mongodb://localhost:27017/e-commerce";
+const connectionString =
+  process.env.MONGODB_URI || "mongodb://localhost:27017/e-commerce";
 
 const configOptions = {
   useNewUrlParser: true,
